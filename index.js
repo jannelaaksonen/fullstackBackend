41a12763d258b28e70092dbf64c091209dcaf4ab
jannelaaksonen <jannelaaksonen86@gmail.com>
2023-12-21 08:48:52 +0200

    3.6 valmis

--- index.js
+++ index.js
@@ -54,28 +54,32 @@ app.delete('/api/persons/:id', (request, response) => {
   })
   
 const generateId = () => {
-const maxId = persons.length > 0
-    ? Math.max(...persons.map(p => p.id))
-    : 0
-return maxId + 1
+    min = Math.ceil(1);
+    max = Math.floor(1000000);
+    const id = Math.floor(Math.random() * (max - min) + min)
+return id
 }
 
 app.post('/api/persons', (request, response) => {
 const body = request.body
 
-if (!body.content) {
+if (!body.name || !body.number) {
     return response.status(400).json({ 
     error: 'content missing' 
-    })
-}
+    })}
+if (persons.find(person => person.name === body.name)) {
+        return response.status(400).json({ 
+        error: 'name must be unique' 
+        })}
+
 
 const person = {
+    id: generateId(),
     name: body.name,
     number: body.number,
-    id: generateId(),
 }
 
-persons = personss.concat(person)
+persons = persons.concat(person)
 
 response.json(person)
 })
