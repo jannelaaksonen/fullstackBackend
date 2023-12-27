4c62d4efd0468ab32ec03754ecd3d53776d08d2d
jannelaaksonen <jannelaaksonen86@gmail.com>
2023-12-27 10:55:02 +0200

    3.12 valmis

---
+++ puhelinluettelo/mongo.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+const noteSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model('Person', noteSchema);
+
+if (process.argv.length === 3) {
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person);
+    });
+    mongoose.connection.close();
+  });
+}
+
+if (process.argv.length > 3) {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  });
+
+  person.save().then(result => {
+    console.log(`Added ${process.argv[3]} number ${process.argv[4]} to phonebook`);
+    mongoose.connection.close();
+  });
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://jannelaaksonen:${password}@cluster0.ijjekgr.mongodb.net/puhelinluettelo?retryWrites=true&w=majority`;
+
+mongoose.set('strictQuery', false);
+mongoose.connect(url);
