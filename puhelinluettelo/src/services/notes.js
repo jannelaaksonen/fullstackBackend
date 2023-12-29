01fa89195184f4c0d9b5965b66f8dc623b743343
jannelaaksonen <jannelaaksonen86@gmail.com>
2023-12-29 09:54:16 +0200

    3.12 valmis

--- puhelinluettelo/src/services/notes.js
+++ puhelinluettelo/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = '/api/persons';
+const baseUrl = 'mongodb+srv://jannelaaksonen:jannelaaksonen@cluster0.ijjekgr.mongodb.net/?retryWrites=true&w=majority';
 
 const getAll = () => {
     return axios.get(baseUrl)
